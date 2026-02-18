2cf280738196414d2370ece43ea0cc579e21ab7c
김한성 <hsung0714@gmail.com>
2026-02-18 16:14:05 +0900

    Feat: cover up firebase open api key

--- src/firebase.ts
+++ src/firebase.ts
@@ -5,13 +5,13 @@ import { getFirestore } from 'firebase/firestore';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAj7acAlG7DDJa60RAkPfGx0_zO2Q55aSs",
-  authDomain: "readly-ff87c.firebaseapp.com",
-  projectId: "readly-ff87c",
-  storageBucket: "readly-ff87c.firebasestorage.app",
-  messagingSenderId: "911494512290",
-  appId: "1:911494512290:web:2385fc23e76efbcdbe61fb",
-  measurementId: "G-F8QT65TSJ2"
+  apiKey: process.env.EXPO_PUBLIC_FIREBASE_API_KEY!,
+  authDomain: process.env.EXPO_PUBLIC_FIREBASE_AUTH_DOMAIN!,
+  projectId: process.env.EXPO_PUBLIC_FIREBASE_PROJECT_ID!,
+  storageBucket: process.env.EXPO_PUBLIC_FIREBASE_STORAGE_BUCKET!,
+  messagingSenderId: process.env.EXPO_PUBLIC_FIREBASE_MESSAGING_SENDER_ID!,
+  appId: process.env.EXPO_PUBLIC_FIREBASE_APP_ID!,
+  measurementId: process.env.EXPO_PUBLIC_FIREBASE_MEASUREMENT_ID!,
 };
 
 const app = initializeApp(firebaseConfig);
